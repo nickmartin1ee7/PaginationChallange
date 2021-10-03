0a378b54f9378c0c2d88160ee26eb17bc63f470e
Nick <nickmartin1ee7@gmail.com>
2021-10-03 15:00:15 -0400

    CSV parsing works

---
+++ PaginationChallange/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PaginationChallange
+{
+    public class Person
+    {
+        public uint Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ PaginationChallange/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace PaginationChallange
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = ReadPeopleFromCsv("targets.csv");
+
+            PaginatedMenu();
+        }
+
+        private static void PaginatedMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<Person> ReadPeopleFromCsv(string targetsCsv)
+        {
+            var people = new List<Person>();
+
+            using TextFieldParser parser = new(targetsCsv);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+
+            bool isHeader = true;
+            
+            while (!parser.EndOfData)
+            {
+                if (isHeader)
+                {
+                    _ = parser.ReadFields();
+                    isHeader = false;
+                    continue;
+                }
+                
+                var fields = parser.ReadFields();
+
+                var salary = fields[4]
+                    .Replace("$", "")
+                    .Replace(",", "");
+
+                var birthDate = string.IsNullOrWhiteSpace(fields[3])
+                    ? default
+                    : DateTime.Parse(fields[3]);
+                
+                people.Add(new Person
+                {
+                    Id = uint.Parse(fields[0]),
+                    FirstName = fields[1],
+                    LastName = fields[2],
+                    BirthDate = birthDate,
+                    Salary = decimal.Parse(salary)
+                });
+            }
+
+            return people;
+        }
+    }
+}
